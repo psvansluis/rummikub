887f7bc35944f9b30a21edad79eae94961ddd8ea
Paulus van Sluis <pvsluis@sogyo.nl>
2022-11-11 17:02:07 +0100

    init frontend

---
+++ svelte-rummikub/src/App.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import Welkom from "./lib/Welkom.svelte";
+  import StartRummikub from "./lib/StartRummikub.svelte";
+  import Spelregels from "./lib/Spelregels.svelte";
+
+  export let menu = 1;
+</script>
+
+<header>
+  <ul id="menu">
+    <li><a href="/" on:click|preventDefault={() => (menu = 1)}>Welkom</a></li>
+    <li>
+      <a href="/" on:click|preventDefault={() => (menu = 2)}>Start</a>
+    </li>
+    <li>
+      <a href="/" on:click|preventDefault={() => (menu = 3)}>Spelregels</a>
+    </li>
+  </ul>
+</header>
+
+<main>
+  {#if menu === 1}
+    <Welkom />
+  {:else if menu === 2}
+    <StartRummikub />
+  {:else if menu === 3}
+    <Spelregels />
+  {:else}
+    <h1>Pagina niet gevonden.</h1>
+  {/if}
+</main>
+
+<style>
+</style>
---
+++ svelte-rummikub/src/lib/Welkom.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+</script>
+
+<h1>Rummikub</h1>
+<p>
+  Hier kun je rummikubben. Gebruik het menu om het spel te spelen of om de
+  spelregels te leren.
+</p>
