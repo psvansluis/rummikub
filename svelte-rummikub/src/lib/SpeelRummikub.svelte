ff224e1bb1b201379d56fb2cd00aa3bf9d217291
Paulus van Sluis <pvsluis@sogyo.nl>
2022-11-22 15:52:49 +0100

    Plankje Component

--- svelte-rummikub/src/lib/SpeelRummikub.svelte
+++ svelte-rummikub/src/lib/SpeelRummikub.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
   import type { SpelStatus } from "../types/SpelStatus.type";
-  import Steen from "./componenten/Steen.svelte";
+  import Plankje from "./componenten/Plankje.svelte";
 
   export let spelStatus: SpelStatus;
 </script>
 
 <div>
-  {#each spelStatus.plankje.stenen as steen}
-    <Steen {steen} />
-  {/each}
+  <Plankje plankje={spelStatus.plankje} />
 </div>
---
+++ svelte-rummikub/src/lib/componenten/Plankje.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import type { Plankje } from "../../types/SpelStatus.type";
+  import Steen from "./Steen.svelte";
+
+  export let plankje: Plankje;
+</script>
+
+<div>
+  {#each plankje.stenen as steen}
+    <Steen {steen} />
+  {/each}
+</div>
