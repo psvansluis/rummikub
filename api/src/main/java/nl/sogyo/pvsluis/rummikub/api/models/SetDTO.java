0d4fefe05bc81e6f2aa330048352701e190a4d4f
psvansluis <psvansluis@users.noreply.github.com>
2022-11-24 12:58:56 +0100

    refactor set-dto

--- api/src/main/java/nl/sogyo/pvsluis/rummikub/api/models/SetDTO.java
+++ api/src/main/java/nl/sogyo/pvsluis/rummikub/api/models/SetDTO.java
@@ -1,27 +1,14 @@
 package nl.sogyo.pvsluis.rummikub.api.models;
 
-import java.util.ArrayList;
-
 import nl.sogyo.pvsluis.rummikub.domain.Rummikub;
 
-public class SetDTO {
-    private SteenDTO[] stenen;
+public class SetDTO extends StenenContainerDTO {
     private boolean isValide;
 
     public SetDTO(Rummikub spel, int setIndex) {
-        ArrayList<ArrayList<Integer>> container = spel.getStenenOpTafel()
-                .get(setIndex);
-        this.stenen = new SteenDTO[container.size()];
-        for (int i = 0; i < stenen.length; i++) {
-            stenen[i] = new SteenDTO(
-                    container.get(i).get(0),
-                    container.get(i).get(1));
-        }
+        super(spel.getStenenOpTafel()
+                .get(setIndex));
         this.isValide = spel.getValiditeitSets().get(setIndex);
-    }
-
-    public SteenDTO[] getStenen() {
-        return stenen;
     }
 
     public boolean isValide() {
