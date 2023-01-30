070a2d4073444dd351237bf34aecef875cc024ab
Paulus van Sluis <pvsluis@sogyo.nl>
2023-01-30 17:01:19 +0100

    client code (niet af)

---
+++ svelte-rummikub/src/lib/componenten/JokerSetter.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  let invoerCijfer: number = 1;
+  let invoerKleur: number = 0;
+
+  function setJoker(): void {
+    dispatch("setJoker", { cijfer: invoerCijfer, kleur: invoerKleur });
+  }
+</script>
+
+<div>
+  <h2>Joker</h2>
+  <label
+    >Cijfer: <input type="number" bind:value={invoerCijfer} min="1" max="13" />
+  </label>
+  <label
+    >Kleur: <input
+      type="number"
+      bind:value={invoerKleur}
+      min="0"
+      max="3"
+    /></label
+  >
+  <button on:click={setJoker}>Pas aan</button>
+</div>
+
+<style>
+  div {
+    padding: 5px;
+    margin: 5px;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
